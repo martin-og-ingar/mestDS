af68d262562cc34b0b5a384e1c57714b2c84a042
Martin <70632679+martinbolle@users.noreply.github.com>
2024-09-12 10:58:08 +0200

    Updated order to make it more logic

--- scripts/testing.py
+++ scripts/testing.py
@@ -2,7 +2,7 @@ from mestDS import generate_data, graph, calculate_weekly_averages
 import datetime
 
 start_date = datetime.date(2024, 1, 1)
-data = generate_data("Uganda", True, 100, start_date, "W")
+data = generate_data("Uganda", True, start_date, 1000, "W")
 graph(
     data,
     "Uganda",
