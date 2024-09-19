02a9b1a6716a2d94945e15fb0df3a5765f85acb3
Martin <70632679+martinbolle@users.noreply.github.com>
2024-09-19 13:11:58 +0200

    Minor adjustment for testing

--- scripts/testing.py
+++ scripts/testing.py
@@ -2,7 +2,7 @@ from mestDS import generate_data, graph, calculate_weekly_averages
 import datetime
 
 start_date = datetime.date(2024, 1, 1)
-data = generate_data("Uganda", True, start_date, 1000, "W")
+data = generate_data("Uganda", True, start_date, 100, "D")
 graph(
     data,
     "Uganda",
@@ -11,6 +11,7 @@ graph(
     precipitation_enabled=True,
 )
 
+# should assure that the data-generations is Week for this to be run.
 average_data = calculate_weekly_averages(data, "Uganda")
 graph(
     average_data,
