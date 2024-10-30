c18c4aaccf8bcc38ddb357348b629d2878bfc43b
Martin <70632679+martinbolle@users.noreply.github.com>
2024-10-30 09:24:20 +0100

    removed some updates.

--- scripts/testing.py
+++ scripts/testing.py
@@ -8,12 +8,11 @@ from mestDS import (
 import datetime
 
 start_date = datetime.date(2024, 1, 1)
-sp = [0.4]
-sa = [0.4]
-si = [10, 11]
+sp = [0.5, 0.4, 0.6]
+sa = [0.3, 0.2, 0.4]
+si = [10, 9, 11]
 multiple_data = generate_multiple_datasets(
-    ["Uganda", "Oslo"], True, True, start_date, 100, "D", sp, sa, si
+    ["Uganda", "Oslo"], True, True, start_date, 200, "D", sp, sa, si
 )
 ds_gluon_format = convert_datasets_to_gluonTS(multiple_data)
-# print(multiple_data)
 graph(multiple_data, True, True, False)
