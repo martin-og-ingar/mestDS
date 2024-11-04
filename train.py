de824cac5f04fc8ba9206741f76e666985a1dc45
geirksa <geirksa@ifi.uio.no>
2024-11-04 18:54:56 +0100

    Added command line parsing and such to make it now work when run from CHAP

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import argparse
+
 import joblib
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -19,6 +21,7 @@ def train(csv_fn, model_fn):
 
         X = df[features]
         Y = df['disease_cases']
+        Y = Y.fillna(0)  # set NaNs to zero (not a good solution, just for the example to work)
         model = LinearRegression()
         model.fit(X, Y)
         models[location] = model
@@ -26,9 +29,13 @@ def train(csv_fn, model_fn):
 
     joblib.dump(models, model_fn)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Train a minimalist forecasting model.')
 
-
-
+    parser.add_argument('csv_fn', type=str, help='Path to the CSV file containing input data.')
+    parser.add_argument('model_fn', type=str, help='Path to save the trained model.')
+    args = parser.parse_args()
+    train(args.csv_fn, args.model_fn)
 
 
 
