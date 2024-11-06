2c240757df5229b0d8471c91f27f69a16e4018e7
Martin <70632679+martinbolle@users.noreply.github.com>
2024-11-06 09:22:59 +0100

    minor update.

--- scripts/testing.py
+++ scripts/testing.py
@@ -12,7 +12,7 @@ sp = [0.5, 0.4, 0.6]
 sa = [0.3, 0.2, 0.4]
 si = [10, 9, 11]
 multiple_data = generate_multiple_datasets(
-    ["Uganda", "Oslo"], True, True, start_date, 200, "D", sp, sa, si
+    ["Uganda", "Oslo"], True, True, start_date, 2000, "D", sp, sa, si
 )
 ds_gluon_format = convert_datasets_to_gluonTS(multiple_data)
-graph(multiple_data, True, True, False)
+graph(multiple_data, False, True, False)
