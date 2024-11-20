3371b56a84613942d57a8537c714af3554ee69c5
ingarbenonisen <ibenonisen@gmail.com>
2024-11-20 14:02:01 +0100

    remove testing.py from git

--- scripts/testing.py
+++
@@ -1,6 +0,0 @@
-from mestDS import Simulation
-from chap_core.predictor.model_registry import registry
-
-sim = Simulation()
-sim.simulate()
-sim.chap_evaluation_on_model(registry.get_model("chap_ewars_monthly"))
