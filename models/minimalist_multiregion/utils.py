a0b3fdab7c68435d364f51dfc8c238cd92d4ea9e
ingarbenonisen <ibenonisen@gmail.com>
2024-11-20 14:00:26 +0100

    remove minimalist_multi.. from git

--- models/minimalist_multiregion/utils.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-#Note: this function is here copied in from the repository chap_model_dev_toolkit to make this example repository easier to develop self-contained.
-#For real development, we recommend installing chap_model_dev_toolkit and instead importing this function from there.
-def get_df_per_location(csv_fn: str) -> dict:
-    full_df = pd.read_csv(csv_fn)
-    unique_locations_list = full_df['location'].unique()
-    locations = {location: full_df[full_df['location'] == location] for location in unique_locations_list}
-    return locations
