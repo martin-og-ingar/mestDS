183fe2812454c198ee1180a606b84a07ccb6e9df
Martin <70632679+martinbolle@users.noreply.github.com>
2025-01-20 08:44:17 +0100

    Update documentation

--- src/mestDS/__init__.py
+++
@@ -1,6 +0,0 @@
-from .simulate import (
-    generate_data,
-)
-from .evaluate import evaluate_chap_model
-from .classes import RainSeason, Simulation, MultipleSimulations, Region
-from .utils import train_test_split_csv, plot_data_with_sample_0
