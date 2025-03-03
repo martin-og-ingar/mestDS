cd44fb5504e7d2d6a899435b6e2fde73c6255a78
Martin <70632679+martinbolle@users.noreply.github.com>
2025-03-03 10:19:37 +0100

    Updated test

--- scripts/testing.py
+++ scripts/testing.py
@@ -7,11 +7,10 @@ root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(root_dir)
 
 from mestDS.classes.SimulationDemo import SimulationsDemo
-from mestDS.classes.Simulation import Simulations
 
 ch_sim = SimulationsDemo("scripts/simulation6.yaml")
 ch_sim.simulate()
-# ch_sim.convert_to_csvs("testing_minimalist_multiregion/")
+# ch_sim.convert_to_csvs("testing_minimalist_multiregion2/")
 # ch_sim.eval_chap_model("models/minimalist_multiregion")
 
 # ch_sim.convert_to_csvs("testing_minimalist_example_lag/")
